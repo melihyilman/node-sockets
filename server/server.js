88a3c3cd652bcd3db9966242077969e446a51816
melihyilman <melihyilman@gmail.com>
2021-02-25 16:41:43 +0300

    socketio

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const path = require("path");
-const express = require("express");
+const port = process.env.PORT || 3000;
 
 const publicPath = path.join(__dirname, "../public");
-var app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.get("/", (req, res) => {
+  res.sendFile(publicPath + "/index.html");
+});
 
-app.listen(PORT, () => {
-  console.log(`server started on ..${PORT}`);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("teyteytey", (msg) => {
+    console.log(`user says : ${msg}`);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
 });
