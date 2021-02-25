a0e48cc8b9200bfc32d61ed0cc018ade6c524015
melihyilman <melihyilman@gmail.com>
2021-02-25 16:00:25 +0300

    initial

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname, "../public");
+var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(PORT, () => {
+  console.log(`server started on ..${PORT}`);
+});
